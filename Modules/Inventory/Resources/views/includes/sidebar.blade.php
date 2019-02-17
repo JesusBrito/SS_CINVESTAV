3de236cbf84053284cc6cfc1f05f7913f92a3458
LFCG1996 <lfcg.programador@gmail.com>
2019-02-17 10:09:26 -0600

    iconos en la plantilla

--- Modules/Inventory/Resources/views/includes/sidebar.blade.php
+++ Modules/Inventory/Resources/views/includes/sidebar.blade.php
@@ -22,53 +22,58 @@
             <li class="header">Menú</li>
             <!-- Optionally, you can add icons to the links -->
 
-            <li><a href="#"><i class="fa fa-pencil-square-o"></i> <span>Alertas</span></a></li>
-            <li><a href="#"><i class="fa fa-file-text"></i> <span>Bitácora de desechos</span></a></li>
-            <li><span>Insumos</span></li>
+            <li><a href="#"><i class="fa fa-warning"></i> <span>Alertas</span></a></li>
+            <li><a href="#"><i class="fa fa-book"></i> <span>Bitácora de desechos</span></a></li>
+
+            <li><i class="fa fa-cubes"></i><span>Insumos</span></li>
 
             <li class="treeview">
               <a href="#">
+                <i class="fa fa-flask"></i>
                 <span>Reactivos</span>
                 <span class="pull-right-container">
                   <i class="fa fa-angle-left pull-right"></i>
                 </span>
               </a>
               <ul class="treeview-menu" style="display: all;">
-                <li class="active"><a href="#"><i class="fa fa-user-plus"></i>Agregar reactivo</a></li>
+                <li class="active"><a href="#"><i class="fa fa-plus-square"></i>Agregar reactivo</a></li>
                 <li><a href="#"><i class="fa fa-list-ul"></i>Listar reactivos</a></li>
               </ul>
             </li>
 
             <li class="treeview">
                     <a href="#">
+                      <i class="fa fa-briefcase"></i>
                       <span>Consumibles</span>
                       <span class="pull-right-container">
                         <i class="fa fa-angle-left pull-right"></i>
                       </span>
                     </a>
                     <ul class="treeview-menu" style="display: all;">
-                      <li class="active"><a href="#"><i class="fa fa-user-plus"></i>Agregar consumible</a></li>
+                      <li class="active"><a href="#"><i class="fa fa-plus-square"></i>Agregar consumible</a></li>
                       <li><a href="#"><i class="fa fa-list-ul"></i>Listar consumibles</a></li>
                     </ul>
             </li>
 
             <li class="treeview">
                     <a href="#">
+                       <i class="fa fa-desktop"></i>
                       <span>Equipos</span>
                       <span class="pull-right-container">
                         <i class="fa fa-angle-left pull-right"></i>
                       </span>
                     </a>
                     <ul class="treeview-menu" style="display: all;">
-                      <li class="active"><a href="#"><i class="fa fa-user-plus"></i>Agregar equipo</a></li>
+                      <li class="active"><a href="#"><i class="fa fa-plus-square"></i>Agregar equipo</a></li>
                       <li><a href="#"><i class="fa fa-list-ul"></i>Listar equipos</a></li>
                     </ul>
             </li>
 
-            <li><span>Gestión de usuarios</span></li>
+            <li><i class="fa fa-users"></i><span>Gestión de usuarios</span></li>
 
             <li class="treeview">
                     <a href="#">
+                      <i class="fa fa-user"></i>
                       <span>Usuarios</span>
                       <span class="pull-right-container">
                         <i class="fa fa-angle-left pull-right"></i>
@@ -80,123 +85,132 @@
                     </ul>
             </li>
 
-            <li><span>Gestión de proveedores</span></li>
+            <li><i class="fa fa-industry"></i><span>Gestión de proveedores</span></li>
 
             <li class="treeview">
                     <a href="#">
+                      <i class="fa fa-suitcase"></i>
                       <span>Proveedores</span>
                       <span class="pull-right-container">
                         <i class="fa fa-angle-left pull-right"></i>
                       </span>
                     </a>
                     <ul class="treeview-menu" style="display: all;">
-                      <li class="active"><a href="#"><i class="fa fa-user-plus"></i>Agregar proveedor</a></li>
+                      <li class="active"><a href="#"><i class="fa fa-plus-square"></i>Agregar proveedor</a></li>
                       <li><a href="#"><i class="fa fa-list-ul"></i>Listar proveedores</a></li>
                     </ul>
             </li>
 
-            <li><span>Catalógos</span></li>
+            <li><i class="fa fa-archive"></i><span>Catalógos</span></li>
 
             <li class="treeview">
                     <a href="#">
+                      <i class="fa fa-asterisk"></i>
                       <span>Toxicidades</span>
                       <span class="pull-right-container">
                         <i class="fa fa-angle-left pull-right"></i>
                       </span>
                     </a>
                     <ul class="treeview-menu" style="display: all;">
-                      <li class="active"><a href="#"><i class="fa fa-user-plus"></i>Agregar toxicidad</a></li>
+                      <li class="active"><a href="#"><i class="fa fa-plus-square"></i>Agregar toxicidad</a></li>
                       <li><a href="#"><i class="fa fa-list-ul"></i>Listar toxicidades</a></li>
                     </ul>
             </li>
 
             <li class="treeview">
                     <a href="#">
+                      <i class="fa fa-flask"></i>
                       <span>Tipos de reactivos</span>
                       <span class="pull-right-container">
                         <i class="fa fa-angle-left pull-right"></i>
                       </span>
                     </a>
                     <ul class="treeview-menu" style="display: all;">
-                      <li class="active"><a href="#"><i class="fa fa-user-plus"></i>Agregar tipo</a></li>
+                      <li class="active"><a href="#"><i class="fa fa-plus-square"></i>Agregar tipo</a></li>
                       <li><a href="#"><i class="fa fa-list-ul"></i>Listar tipos</a></li>
                     </ul>
             </li>
 
             <li class="treeview">
                     <a href="#">
+                      <i class="fa fa-sun-o"></i>
                       <span>Temperaturas</span>
                       <span class="pull-right-container">
                         <i class="fa fa-angle-left pull-right"></i>
                       </span>
                     </a>
                     <ul class="treeview-menu" style="display: all;">
-                      <li class="active"><a href="#"><i class="fa fa-user-plus"></i>Agregar temperatura</a></li>
+                      <li class="active"><a href="#"><i class="fa fa-plus-square"></i>Agregar temperatura</a></li>
                       <li><a href="#"><i class="fa fa-list-ul"></i>Listar temperaturas</a></li>
                     </ul>
             </li>
 
             <li class="treeview">
                     <a href="#">
+                      <i class="fa fa-location-arrow"></i>
                       <span>Ubicaciones</span>
                       <span class="pull-right-container">
                         <i class="fa fa-angle-left pull-right"></i>
                       </span>
                     </a>
                     <ul class="treeview-menu" style="display: all;">
-                      <li class="active"><a href="#"><i class="fa fa-user-plus"></i>Agregar ubicación</a></li>
+                      <li class="active"><a href="#"><i class="fa fa-plus-square"></i>Agregar ubicación</a></li>
                       <li><a href="#"><i class="fa fa-list-ul"></i>Listar ubicaciones</a></li>
                     </ul>
             </li>
 
             <li class="treeview">
                     <a href="#">
+                      <i class="fa fa-registered"></i>
                       <span>Marcas</span>
                       <span class="pull-right-container">
                         <i class="fa fa-angle-left pull-right"></i>
                       </span>
                     </a>
                     <ul class="treeview-menu" style="display: all;">
-                      <li class="active"><a href="#"><i class="fa fa-user-plus"></i>Agregar marca</a></li>
+                      <li class="active"><a href="#"><i class="fa fa-plus-square"></i>Agregar marca</a></li>
                       <li><a href="#"><i class="fa fa-list-ul"></i>Listar marcas</a></li>
                     </ul>
             </li>
 
             <li class="treeview">
                     <a href="#">
+                      <i class="fa fa-briefcase"></i>
                       <span>Categorías de consumibles</span>
                       <span class="pull-right-container">
                         <i class="fa fa-angle-left pull-right"></i>
                       </span>
                     </a>
                     <ul class="treeview-menu" style="display: all;">
-                      <li class="active"><a href="#"><i class="fa fa-user-plus"></i>Agregar categoría</a></li>
+                      <li class="active"><a href="#"><i class="fa fa-plus-square"></i>Agregar categoría</a></li>
                       <li><a href="#"><i class="fa fa-list-ul"></i>Listar categorías</a></li>
                     </ul>
             </li>
 
             <li class="treeview">
                     <a href="#">
+                      <i class="fa fa-trash-o"></i>
                       <span>Categorías de desechos</span>
                       <span class="pull-right-container">
                         <i class="fa fa-angle-left pull-right"></i>
                       </span>
                     </a>
                     <ul class="treeview-menu" style="display: all;">
-                      <li class="active"><a href="#"><i class="fa fa-user-plus"></i>Agregar categoría</a></li>
+                      <li class="active"><a href="#"><i class="fa fa-plus-square"></i>Agregar categoría</a></li>
                       <li><a href="#"><i class="fa fa-list-ul"></i>Listar categorías</a></li>
                     </ul>
             </li>
 
             <li class="treeview">
                     <a href="#">
+                      <i class="fa fa-sort-numeric-asc"></i>
                       <span>Unidades de volumen</span>
                       <span class="pull-right-container">
                         <i class="fa fa-angle-left pull-right"></i>
                       </span>
                     </a>
                     <ul class="treeview-menu" style="display: all;">
-                      <li class="active"><a href="#"><i class="fa fa-user-plus"></i>Agregar unidad</a></li>
+                      <li class="active"><a href="#"><i class="fa fa-plus-square"></i>Agregar unidad</a></li>
                       <li><a href="#"><i class="fa fa-list-ul"></i>Listar unidades</a></li>
                     </ul>
             </li>
