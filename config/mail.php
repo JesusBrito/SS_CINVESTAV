634e00f0ed2762ddbac77d3951517fdae9d92323
LFCG1996 <lfcg.programador@gmail.com>
2019-02-16 18:30:32 -0600

    checando correo

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'lfcg.programador@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'CINVESTAV'),
     ],
 
     /*
